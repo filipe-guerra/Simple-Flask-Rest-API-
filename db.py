a33ad1b53d624da4ded1ff1a2fef29fcf2dd84ec
Filipe Guerra <filipehmguerra@yahoo.com.br>
2019-02-10 16:22:10 -0200

    Correcao na programacao do Banco de dados

--- db.py
+++ db.py
@@ -1,32 +1,48 @@
 # -*- coding: utf-8 -*-
 import sqlite3
 
+
 class database:
     def __init__(self):
-        
-        self.conn = sqlite3.connect('database.sqlite')
+        self.database = 'database.sqlite' 
+
+        self.conn = sqlite3.connect(self.database)
         self.cur = self.conn.cursor()
 
-        self.cur.execute(
-        'DROP TABLE IF EXISTS devices;')
+# Remove the table 
+#        self.cur.execute(
+#        'DROP TABLE IF EXISTS devices;')
 
         self.cur.execute('''
-        CREATE TABLE devices (
+        CREATE TABLE IF NOT EXISTS devices (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         deviceName TEXT UNIQUE NOT NULL,
         deviceStatus BOOLEAN NOT NULL
         );''')
 
+        self.close()
+
+    def connect(self):
+        self.conn = sqlite3.connect(self.database)
+        self.cur = self.conn.cursor()
+
+    def close(self):
+        self.cur.close()
+
     def addDevice(self, name):
-        return self.cur.execute('''
+        self.connect()
+        aux = self.cur.execute('''
         INSERT INTO devices VALUES(
             NULL,
             '{0}',
             0
         );'''.format(name))
         self.conn.commit()
+        self.close()
+        return aux
 
     def getAllDevices(self):
+        self.connect()
         allitens = self.cur.execute(
         'SELECT * FROM devices;')
 
@@ -34,34 +50,41 @@ class database:
         for item in allitens:
             func = lambda x : 'on' if (x) else 'off'
             devc[item[1]] = {'status': func(item[2])}
+        
+        self.close()
         return devc
         
     def getDevice(self, deviceNm):
+        self.connect()
         busca = self.cur.execute(
         'SELECT * FROM devices WHERE deviceName=:name;',
         {"name": deviceNm})
-
         devc = {}
         for item in busca:
             func = lambda x : 'on' if (x) else 'off'
             devc[item[1]] = {'status': func(item[2])}
+        
+        self.close()
         return devc
 
     def updateDevice(self, deviceNm, status):
+        self.connect()
         func = lambda x: 1 if (x=='on') else 0
-        return self.cur.execute(
+        aux = self.cur.execute(
         'UPDATE devices SET deviceStatus=:stat WHERE deviceName=:name;',
         {"stat": func(status), "name": deviceNm})
         self.conn.commit()
+        self.close()
+        return aux
 
     def removeDevice(self, deviceNm):
-        return self.cur.execute(
+        self.connect()
+        aux = self.cur.execute(
         'DELETE FROM devices WHERE deviceName=:name;',
         {"name": deviceNm})
         self.conn.commit()
-
-    def close(self):
-        self.cur.close()
+        self.close()
+        return aux
 
 dbsq = database()
 
